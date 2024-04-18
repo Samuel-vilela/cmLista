770e126834c08f760daa1bc60493fff1753f8148
samuel.varaujo <samuel.varaujo@AME0613368W10-1>
2024-04-17 22:16:44 -0300

    Adicionar arquivos de projeto.

---
+++ cmLista/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace cmLista
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxname = new System.Windows.Forms.TextBox();
+            this.cmblista = new System.Windows.Forms.ComboBox();
+            this.btnRenovar = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // textBoxname
+            // 
+            this.textBoxname.Location = new System.Drawing.Point(108, 54);
+            this.textBoxname.Name = "textBoxname";
+            this.textBoxname.Size = new System.Drawing.Size(298, 26);
+            this.textBoxname.TabIndex = 1;
+            // 
+            // cmblista
+            // 
+            this.cmblista.FormattingEnabled = true;
+            this.cmblista.Location = new System.Drawing.Point(108, 108);
+            this.cmblista.Name = "cmblista";
+            this.cmblista.Size = new System.Drawing.Size(298, 28);
+            this.cmblista.TabIndex = 2;
+            this.cmblista.SelectedIndexChanged += new System.EventHandler(this.cmlista_SelectedIndexChanged);
+            // 
+            // btnRenovar
+            // 
+            this.btnRenovar.Location = new System.Drawing.Point(108, 158);
+            this.btnRenovar.Name = "btnRenovar";
+            this.btnRenovar.Size = new System.Drawing.Size(91, 41);
+            this.btnRenovar.TabIndex = 3;
+            this.btnRenovar.Text = "Renovar";
+            this.btnRenovar.UseVisualStyleBackColor = true;
+            this.btnRenovar.Click += new System.EventHandler(this.btnRenovar_Click);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(322, 159);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(84, 40);
+            this.btnAdicionar.TabIndex = 4;
+            this.btnAdicionar.Text = "adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.btnRenovar);
+            this.Controls.Add(this.cmblista);
+            this.Controls.Add(this.textBoxname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxname;
+        private System.Windows.Forms.ComboBox cmblista;
+        private System.Windows.Forms.Button btnRenovar;
+        private System.Windows.Forms.Button btnAdicionar;
+    }
+}
+
---
+++ cmLista/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cmLista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmlista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmblista.SelectedIndex = 0;
+
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (textBoxname.TextLength == 0)
+                MessageBox.Show("Por favor digite algo!", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            textBoxname.Focus();
+
+            {
+                cmblista.Items.Add(textBoxname.Text);
+                textBoxname.Clear();
+                textBoxname.Focus();
+            }
+        }
+
+        private void btnRenovar_Click(object sender, EventArgs e)
+        {
+            if (cmblista.SelectedIndex == 0)
+                MessageBox.Show("Por favor selecione um item", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            {
+                cmblista.Items.Remove(cmblista.SelectedItem);
+                cmblista.SelectedIndex = 0;
+            }
+        }
+    }
+}
